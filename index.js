4a060f09666c031fe73f9149f37977f0cb10b8fa
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-13 09:12:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var customerName = "bob";
 var bestCustomer;
 
+// declare customerName to be bob in global scope
 function upperCaseCustomerName() {
-  return customerName.toUpperCase();
+  customerName = customerName.toUpperCase();
 }
 
+// setBestCustomer
 function setBestCustomer() {
   bestCustomer = "not bob";
 }
 
+// overwriteBestCustomer: overwrites the best customer
 function overwriteBestCustomer() {
   return bestCustomer = "maybe bob";
 }
 
+// changeLeastFavoriteCustomer: unsuccessfully tries to reassign the least favorite customer
 const leastFavoriteCustomer = "bob";
 function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = "not bob";
