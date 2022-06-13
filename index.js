3a43a05a5edfa25afbbd0326d317a4b5e070e237
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-13 09:12:20 +0300

    commit tree e31c17c440a0ef6abf3b5f230698cc6ef011371c

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 var customerName = "bob";
 var bestCustomer;
 
+// declare customerName to be bob in global scope
+// Fix the Scope
+//        upperCaseCustomerName()
+//          modifies the customerName variable:
+
+//       AssertionError: expected 'bob' to equal 'BOB'
+//       + expected - actual
+
+//       -bob
+//       +BOB
+
 function upperCaseCustomerName() {
-  return customerName.toUpperCase();
+  customerName = customerName.toUpperCase();
 }
 
+// function upperCaseCustomerName() {
+//   return customerName.toUpperCase();
+// }
+
+
+// setBestCustomer
 function setBestCustomer() {
   bestCustomer = "not bob";
 }
 
+// overwriteBestCustomer: overwrites the best customer
 function overwriteBestCustomer() {
   return bestCustomer = "maybe bob";
 }
 
+// changeLeastFavoriteCustomer: unsuccessfully tries to reassign the least favorite customer
 const leastFavoriteCustomer = "bob";
 function changeLeastFavoriteCustomer() {
   leastFavoriteCustomer = "not bob";
